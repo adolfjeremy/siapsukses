e6f66bf3129a6b0ac0ccbf17c027f62504bbbede
adolfjeremy <adolf.jer@email.com>
2026-02-13 14:44:40 +0700

    adding sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
+  site: "https://siapsukses.com", // WAJIB isi domain production
+  integrations: [sitemap()],
   output: "static",
   build: {
     format: "file",
